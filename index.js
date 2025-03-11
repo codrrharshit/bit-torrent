2216c89950343cda1b7da0b304d1aae9ea6af6ba
Harshit Grover <codrrharshit@gmail.com>
2025-03-11 19:53:39 +0530

    working on track url

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import {readFileSync} from 'node:fs'
+import bencode from 'bencode'
+import { Buffer } from 'node:buffer'
+import dgram from 'dgram'
+import url from 'url'
 
+
+
+ 
 const content = readFileSync("puppy.torrent");
-console.log(content.toString('utf-8'));
+const torrent = bencode.decode(content,'utf-8');
+const URL = url.parse(torrent.announce.toString('utf-8'));
+console.log(URL);
+
+const socket= dgram.createSocket('udp4');
+const myMsg=Buffer.from('hello?','utf-8')
+
+socket.send(myMsg,0,myMsg.length,URL.port,URL.host,()=>{});
+socket.on('message',(msg)=>{
+    console.log("Message is  ",msg);
+})
+
 
